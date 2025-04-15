017618944ed0efc925a42ce482da32ff37690a17
Daniil Serov <alnmlbch@gmail.com>
2025-04-15 20:48:35 +0300

    fix(bot): update controller test

--- bot/src/test/java/backend/academy/bot/controller/SimpleBotControllerTest.java
+++ bot/src/test/java/backend/academy/bot/controller/SimpleBotControllerTest.java
@@ -1,9 +1,5 @@
 package backend.academy.bot.controller;
 
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyLong;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -11,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import backend.academy.base.schema.bot.LinkUpdate;
 import backend.academy.bot.BotTest;
-import backend.academy.bot.sender.BotMessageSender;
+import backend.academy.bot.link.service.LinkUpdateService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -28,7 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 public class SimpleBotControllerTest implements BotTest {
 
     @MockitoBean
-    private BotMessageSender bot;
+    private LinkUpdateService linkUpdateService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -41,12 +37,11 @@ public class SimpleBotControllerTest implements BotTest {
         final LinkUpdate update = new LinkUpdate(1L, URL, "", List.of(3L, 4L));
 
         mockMvc.perform(post("/updates")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(update)))
-                .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(update)))
+            .andExpect(status().isOk());
 
-        verify(bot, times(1)).sendUpdate(eq(3L), any(), any());
-        verify(bot, times(1)).sendUpdate(eq(4L), any(), any());
+        verify(linkUpdateService, times(1)).update(update);
     }
 
     @Test
@@ -54,11 +49,11 @@ public class SimpleBotControllerTest implements BotTest {
         final LinkUpdate update = new LinkUpdate(1L, URL, "", List.of());
 
         mockMvc.perform(post("/updates")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(update)))
-                .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(update)))
+            .andExpect(status().isOk());
 
-        verify(bot, never()).sendUpdate(anyLong(), any(), any());
+        verify(linkUpdateService, times(1)).update(update);
     }
 
     @Test
@@ -66,6 +61,6 @@ public class SimpleBotControllerTest implements BotTest {
         final String invalidJson = "{ \"id\": 1, \"url\": [] }";
 
         mockMvc.perform(post("/updates").contentType(MediaType.APPLICATION_JSON).content(invalidJson))
-                .andExpect(status().isBadRequest());
+            .andExpect(status().isBadRequest());
     }
 }
