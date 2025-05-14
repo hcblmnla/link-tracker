25b35bf5d676e0dfc3184083776cb0c04a693617
Daniil Serov <alnmlbch@gmail.com>
2025-05-14 21:01:40 +0300

    refactor: add missing semicolon

--- migrations/sql/02-user-tags.sql
+++ migrations/sql/02-user-tags.sql
@@ -3,4 +3,4 @@ create table user_tags
     user_id bigint references users (id) on delete cascade,
     tag_id  bigint references tags (id) on delete cascade,
     primary key (user_id, tag_id)
-)
+);
